d08ab848f3a5992314b642fecd3687885b1d073b
Trevor Stubbs <stubbste@gmail.com>
2020-05-12 14:48:12 -0700

    finish chart

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ let userDefinedRounds = 25;
 let lastViewed = [];
 
 // Arrays to populate the chart
-var name = [];
+var names = [];
 var votes = [];
 
 // Image object constructor
@@ -113,6 +113,15 @@ function outputChartData(){
   }
 }
 
+// Functions to fill the chart arrays
+function fillChartArrays(){
+  for(let i = 0; i < allImages.length; i++){
+    names.push(allImages[i].title);
+    votes.push(allImages[i].votes);
+  }
+  generateChart();
+}
+
 // Global Random Number Gen (Exclusive)
 function randomNumber(min, max) {
   return Math.floor(Math.random()* (max-min));
@@ -132,6 +141,7 @@ parentElement.addEventListener('click', function handler() {
   // Remove the listener when the rounds are finished
   userDefinedRounds--;
   if(userDefinedRounds <= 0){
+    fillChartArrays();
     outputChartData();
     this.removeEventListener('click', handler);
     // Remove images to reduce user confusion
@@ -148,3 +158,81 @@ parentForm.addEventListener('submit', function(event){
   userDefinedRounds = rounds;
   console.log(rounds);
 });
+
+// export {names};
+// export {votes};
+function generateChart(){
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: names,
+      datasets: [{
+        label: '# of Votes',
+        data: votes,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+}
